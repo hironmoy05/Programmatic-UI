41b53b1acdb84145500152c0943dc27fc882831e
gauravzone <gauravdhar35@gmail.com>
2023-07-08 00:01:29 +0530

    Added FirstScreen Button and SecondScreen with Programmatic

---
+++ Programatic-UI/SecondScreen.swift
@@ -0,0 +1,17 @@
+//
+//  SecondScreen.swift
+//  Programatic-UI
+//
+//  Created by Gaurav Sara on 16/04/1945 Saka.
+//
+
+import UIKit
+
+class SecondScreen: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemMint
+    }
+}
